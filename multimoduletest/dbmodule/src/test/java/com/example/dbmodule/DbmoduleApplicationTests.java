cbc5ac7f85ed6e09fa07b8b58f7379d7cd238aa9
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-08-30 01:39:43 +0900

    dbmodule 추가

---
+++ multimoduletest/dbmodule/src/test/java/com/example/dbmodule/DbmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
