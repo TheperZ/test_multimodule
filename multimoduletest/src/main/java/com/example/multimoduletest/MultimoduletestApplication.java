5ea300aa6bd4e93d425df19b22d138284b7a52b8
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-08-30 00:06:24 +0900

    init project

---
+++ multimoduletest/src/main/java/com/example/multimoduletest/MultimoduletestApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimoduletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimoduletestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimoduletestApplication.class, args);
+	}
+
+}
