3918f0a20725038bd8b77dcea063ae2a511faedd
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-08-30 22:41:04 +0900

    dao 구현

---
+++ multimoduletest/dbmodule/src/main/java/com/dbmodule/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.dbmodule.repository;
+
+import com.dbmodule.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByTitle(String title);
+
+}
