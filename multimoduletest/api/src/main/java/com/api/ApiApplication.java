18f277aa124529f314772d95ca5fe27ccf9d5abd
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-08-31 00:29:45 +0900

    yml 파일을 애플리케이션에서 읽도록 수정

--- multimoduletest/api/src/main/java/com/api/ApiApplication.java
+++ multimoduletest/api/src/main/java/com/api/ApiApplication.java
@@ -3,10 +3,11 @@ package com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.dbmodule")
+@SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-api,application-dbmodule");
         SpringApplication.run(ApiApplication.class, args);
     }
 
